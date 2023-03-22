fe9b922a83fcc9ac87a3261ea60f626d6f941054
Дмитрий <dimon-52rus@ya.ru>
2023-03-22 20:52:25 +0300

    add pages and page locators

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+    FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
+    EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR, "input[id='currentAddress']")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR, "input[id='permanentAddress']")
+    SUBMIT = (By.CSS_SELECTOR, "input[id='submit']")
+
+    CREATED_FULL_NAME = (By.CSS_SELECTOR, "#output #name")
+    CREATED_EMAIL = (By.CSS_SELECTOR, "#output #email")
+    CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, "#output #currentAddress")
+    CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, "#output #permanentAddress")
