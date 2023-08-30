8dc664f107f7b2c0b601a4bb15284644559c15f4
Дмитрий <dimon-52rus@ya.ru>
2023-08-30 14:53:26 +0300

    import driver from conftest to tests

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def driver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome()
     driver.maximize_window()
     yield driver
     driver.quit()
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,7 @@
 import time
+from conftest import driver
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
-from pages.elements_page import TextBoxPage
 
 class TestElements:
     class TestTextBox:
@@ -10,6 +11,17 @@ class TestElements:
             input_data = text_box_page.fill_all_fields()
             time.sleep(5)
             output_data = text_box_page.check_filled_form()
-            assert input_data == output_data
+            assert input_data == output_data, "mismatch input data and output data!"
 
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            print(input_checkbox, output_result)
+            assert input_checkbox == output_result
 
+            time.sleep(5)
